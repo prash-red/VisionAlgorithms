efbd7cf8d60d8df95cb2e2265609ef599e716af7
prash-red <shyamalaprashanth2004@gmail.com>
2025-07-03 21:09:12 -0400

    Fix another CMake error

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,10 +2,10 @@
 file(
         DOWNLOAD
         https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.42.0/CPM.cmake
-        CPM.cmake
+        ${CMAKE_BINARY_DIR}/CPM.cmake
         EXPECTED_HASH SHA256=2020b4fc42dba44817983e06342e682ecfc3d2f484a581f11cc5731fbe4dce8a
 )
-include(${CMAKE_CURRENT_BINARY_DIR}/CPM.cmake)
+include(${CMAKE_BINARY_DIR}/CPM.cmake)
 
 # add dependencies here
 CPMAddPackage(
