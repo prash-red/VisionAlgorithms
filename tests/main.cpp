8e6c4eee8b2b69dc33ed5ec05a98ab3c2d61872b
Prashanth Reddy Shyamala <111243248+prash-red@users.noreply.github.com>
2025-07-10 21:42:35 -0400

    Update test library to Catch2 (#8)

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,5 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
+#include <catch2/catch_test_macros.hpp>
 
-// This is all that is needed to compile a test-runner executable.
-// More tests can be added here, or in a new tests/*.cpp file.
+TEST_CASE("simple test") {
+    REQUIRE(1 + 1 == 2);
+}
